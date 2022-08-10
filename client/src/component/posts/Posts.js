6211bb11194367b56c79c3aba0352db24d3410f3
salammustafa728 <salammustafa728@gmail.com>
2022-08-10 19:39:58 +0300

    add post cards

--- client/src/component/posts/Posts.js
+++ client/src/component/posts/Posts.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Post from "./post/Post";
+import { Grid, CircularProgress } from "@mui/material";
 import { useSelector } from "react-redux";
-// import useStyles from './Styles'
+import useStyles from './Styles'
 const Posts = () =>{
   const post = useSelector((state)=>state.post)
-    // const classes = useStyles();
+    const classes = useStyles();
 
     console.log(post);
     return(
-        <>
-          <h1>Posts</h1>
-          <Post/>
-          <Post/>
-        </>
-      
+       !post.length ? <CircularProgress/> : (
+       <Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+          {
+            post.map((post)=>(
+              <Grid key={post._id} item xs={12} sm={6}>
+                  <Post post={post} />
+              </Grid>
+            ))
+          }
+       </Grid>
+       )
     )
 }
 
-
+// xs for mobile and small devices / 12 mean all the space/ sm for small and larger
 export default Posts;
