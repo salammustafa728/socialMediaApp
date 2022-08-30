a83b4ef4dbcabde080d34abafd03db983ecd0efe
salammustafa728 <salammustafa728@gmail.com>
2022-08-30 15:55:09 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({limit:'30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
 
@@ -14,8 +14,12 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://salammustafa728:root@cluster0.t6y17nj.mongodb.net/?retryWrites=true&w=majority';
+app.use('/',(req,res)=>{
+    res.send('Hello to memories API');
+})
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
+//'mongodb+srv://salammustafa728:root@cluster0.t6y17nj.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
